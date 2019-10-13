b6fb0245cdc9a4f62f799802c21e2c8f3a9b99c8
Martuza Shimul <martuza.shimul@gmail.com>
2019-10-14 03:19:50 +0600

    delete function added

--- src/App.js
+++ src/App.js
@@ -8,11 +8,21 @@ class App extends Component {
       { id: 2, content: "Complete React with Firebase" }
     ]
   };
+
+  deleteTodo = id => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos // *ES6 shorhand, mainly it means todos: todos
+    });
+  };
+
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
   }
