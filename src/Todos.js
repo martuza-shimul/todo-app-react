b754ca66be1ea89874f1a9e6fa0c77e7a0e2f967
Martuza Shimul <martuza.shimul@gmail.com>
2019-10-14 03:45:56 +0600

    ver 1.0.0

--- src/Todos.js
+++ src/Todos.js
@@ -5,7 +5,13 @@ const Todos = ({ todos, deleteTodo }) => {
     todos.map(todo => {
       return (
         <div className="collection-item" key={todo.id}>
-          <span onClick={() => {deleteTodo(todo.id)}}>{todo.content}</span>
+          <span
+            onClick={() => {
+              deleteTodo(todo.id);
+            }}
+          >
+            {todo.content}
+          </span>
         </div>
       );
     })
