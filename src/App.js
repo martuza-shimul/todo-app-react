11aa18c1a6e2b53bd73c51b9ab0e3e70768f6658
Martuza Shimul <martuza.shimul@gmail.com>
2019-10-14 03:05:16 +0600

    Todos component initialized

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Todos from "./Todos";
 
 class App extends Component {
   state = {
@@ -9,8 +10,9 @@ class App extends Component {
   };
   render() {
     return (
-      <div className="App">
-        <h1>Hello World</h1>
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
